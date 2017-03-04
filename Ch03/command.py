c282a1a3c8b9b95af962bb4a0938a123c1ed0bd1
eyuiwng <eyuiwng@CN00119868.ericsson.se>
2017-03-04 17:38:00 +0800

    commands command

--- Ch03/command.py
+++ Ch03/command.py
@@ -3,13 +3,13 @@
 #from sys import path
 #path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch03')
 
-#Ch3 决策树
+#Ch3 å†³ç­–æ ‘
 
-# 3.1 决策树的构造
+# 3.1 å†³ç­–æ ‘çš„æž„é€ 
 
-# 3.1.1 信息增益
+# 3.1.1 ä¿¡æ�¯å¢žç›Š
 
-# 计算给定数据集的香农熵
+# è®¡ç®—ç»™å®šæ•°æ�®é›†çš„é¦™å†œç†µ
 
 import trees
 myDat, labels = trees.createDataSet()
@@ -26,23 +26,23 @@ print trees.calcShannonEnt(myDat)
 '''
 
 
-# 3.1.2 划分数据集
+# 3.1.2 åˆ’åˆ†æ•°æ�®é›†
 
 print trees.splitDataSet(myDat,0,1)
 print trees.splitDataSet(myDat,0,0)
 
-# 选择最好的数据集划分方式
+# é€‰æ‹©æœ€å¥½çš„æ•°æ�®é›†åˆ’åˆ†æ–¹å¼�
 print trees.chooseBestFeatureToSplit(myDat)
 
-# 3.1.3 递归构建决策树
+# 3.1.3 é€’å½’æž„å»ºå†³ç­–æ ‘
 
 myTree = trees.createTree(myDat,labels)
 print myTree
 
 
-# 3.2 使用Matplotlib注解绘制树形图
+# 3.2 ä½¿ç”¨Matplotlibæ³¨è§£ç»˜åˆ¶æ ‘å½¢å›¾
 
-# 3.2.1 Matplotlib 注解
+# 3.2.1 Matplotlib æ³¨è§£
 
 import treePlotter
 treePlotter.createPlot(myTree)
