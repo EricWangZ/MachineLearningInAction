cc5407a9419d4c05832adf7ff8834fd735fab3b3
RayWang <RayWang@LAPTOP-EK3E5BM7>
2017-03-04 15:16:27 +0800

    Initialization

---
+++ Ch02/command.py
@@ -0,0 +1,26 @@
+import kNN
+group,labels = kNN.createDataSet()
+group
+'reload(kNN)'
+
+
+datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+datingDataMat
+
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import *
+fig = plt.figure()
+ax = fig.add_subplot(111)
+'ax.scatter(datingDataMat[:,1], datingDataMat[:,2])'
+'plt.show()'
+
+ax.scatter(datingDataMat[:,1], datingDataMat[:,2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()
+
+            
+reload(kNN)
+normMat, ranges, minVals = kNN.autoNorm(datingDataMat)
+normMat
+ranges
+minVals
