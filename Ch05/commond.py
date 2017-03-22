9b68cbec079a37cd71b55a9d1c35a4e3e434bc4f
RayWang <RayWang@LAPTOP-EK3E5BM7>
2017-03-22 23:21:20 +0800

    Ch06 first commit

--- Ch05/commond.py
+++ Ch05/commond.py
@@ -1,8 +1,8 @@
 #coding=utf-8
 
 from sys import path
-path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch05')
-#path.append(r'D:\Study\Workspaces\MyEclipse 2015\MachineLearningInAction\Ch05')
+#path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch05')
+path.append(r'D:\Study\Workspaces\MyEclipse 2015\MachineLearningInAction\Ch05')
 
 '''
 import os
