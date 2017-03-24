ffacf319b00f427ff5be75d302e2a37c8e7cdc13
eyuiwng <eyuiwng@CN00119868.ericsson.se>
2017-03-24 18:00:35 +0800

    commit

--- Ch07/command.py
+++ Ch07/command.py
@@ -8,3 +8,13 @@ path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch07')
 from numpy import *
 import Myadaboost
 
+# 第七章 利用AdaBoost元算法提高分类性能
+# 7.1 基于数据集多重抽样的分类器
+# 7.1.1 bagging： 基于数据随机重抽样的分类器构建方法
+# 7.1.2 boosting： 
+
+
+# 7.2 训练算法： 基于错误提升分类器的性能
+
+
+
