7b67a6e9d17bca5ad39648679aca19525f9569dc
RayWang <RayWang@LAPTOP-EK3E5BM7>
2017-03-25 14:25:32 +0800

    commit Ch07 1st

--- Ch07/command.py
+++ Ch07/command.py
@@ -2,8 +2,8 @@
 #coding=utf-8
 
 from sys import path
-path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch07')
-#path.append(r'D:\Study\Workspaces\MyEclipse 2015\MachineLearningInAction\Ch07')
+#path.append(r'C:\Users\eyuiwng\Desktop\Study\machine learning\workspace\Ch07')
+path.append(r'D:\Study\Workspaces\MyEclipse 2015\MachineLearningInAction\Ch07')
 
 from numpy import *
 import Myadaboost
@@ -15,6 +15,41 @@ import Myadaboost
 
 
 # 7.2 训练算法： 基于错误提升分类器的性能
+
+# 7.3 基于单层决策树构建弱分离器
+
+dataMat,classLabels = Myadaboost.loadSimpData()
+
+D = mat(ones((5,1))/5)
+bestStump,minError,bestClasEst = Myadaboost.buildStump(dataMat,classLabels,D)
+print bestStump,minError,bestClasEst
+
+
+# 7.4 完整Adaboost算法的实现
+
+weakClassArr,aggClassEst = Myadaboost.adaBoostTrainDS(dataMat,classLabels,9)
+print weakClassArr
+print aggClassEst
+
+
+dataMat,classLabels = Myadaboost.loadSimpData()
+classifierArray, aggClassEst = Myadaboost.adaBoostTrainDS(dataMat,classLabels,30)
+
+# 7.5 测试算法：基于AdaBoost的分类
+Myadaboost.adaClassify ([0,0], classifierArray)
+Myadaboost.adaClassify ([[5,5],[0,0]], classifierArray)
+
+# 7.6 示例：在一个难数据集上应用AdaBoost
+dataArr,labelArr = Myadaboost.loadDataSet("horseColicTraining2.txt")
+classifierArray,aggClassEst = Myadaboost.adaBoostTrainDS(dataArr,labelArr)
+print classifierArray
+
+testArr,testLabelArr = Myadaboost.loadDataSet("horseColicTest2.txt")
+prediction10=Myadaboost.adaClassify (testArr, classifierArray)
+
+errArr = mat(ones((67,1)))
+print errArr[prediction10 != mat(testLabelArr).T].sum()/67
+
 
 
 
